efa210d933bfb0e410e96e75eaf00585da059a68
carlota <carlota.monederoh@gmail.com>
2022-12-18 16:29:04 +0100

    Documentation for utils.py

--- AEMET/utils.py
+++ AEMET/utils.py
@@ -57,6 +57,17 @@ def input_date(date):
         return input_date(date=input('Please enter a valid date: '))
 
 def write_json(dic, file_path):
+    """
+    Function used to write data to JSON in a specified file_path
+
+    Params:
+    -dic: dictionary
+        Python dictionary to be written to file
+
+    -file_path: string
+        final location of the file
+
+    """
     import json
     with open(file_path, "w") as f:
         json.dump(dic, f)
